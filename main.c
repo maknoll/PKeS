120d7f0fce3378510b476bd3f3b9b043854324f6
Andreas Pfohl <andreas@pf0hl.de>
2012-04-30 16:12:07 +0200

    Fancy LED interrupt switching action. Programable.

--- main.c
+++ main.c
@@ -1,54 +1,95 @@
-/*
- * main.c
- *
- *  Created on: Apr 16, 2012
- *      Author: Sebastian Zug
- *              Eclipse AVR Project
- */
-
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+#include <util/delay.h>
 
-/*! Makros for led control*/
-#define ledOn(x)  (PORTA |= (1<<x))
-#define ledOff(x) (PORTA &=~(1<<x))
-#define button(x) ((PINA & (16<<x))==0)
+/* Makros for led control */
+#define ledOn(x) (PORTA |= (1<<x))
+#define ledOff(x) (PORTA &= ~(1<<x))
+#define button(x) ((PINA & (16<<x)) == 0)
 
-/*! Robby Board Specification*/
-//-------------------------------- Port A -------------------------------
-#define LED_0		0
-#define LED_1		1
-#define LED_2		2
-#define LED_3		3
-#define BUTTON_0	4
-#define BUTTON_1	5
-#define BUTTON_2	6
-#define BUTTON_3	7
+/* Robby Board Specification */
+#define LED_0 0
+#define LED_1 1
+#define LED_2 2
+#define LED_3 3
+#define BUTTON_0 4
+#define BUTTON_1 5
+#define BUTTON_2 6
+#define BUTTON_3 7
 
-void init(){
-	//-------------------------------- Port A -------------------------------
-	// set leds LED_X as output
-	// alternative
-	//     - intermediate
-	//       DDRA=1+2+4+8;
-	//     - using processor makros
-	//       DDRA=((1<<DDA0) | (1<<DDA1) | (1<<DDA2) | (1<<DDA3));
-	//     - using hardware specific makros
+typedef struct
+{
+	int length;
+	int state;
+	int *leds;
+} program_t;
+
+#define PROGRAMS 2
+
+program_t program[PROGRAMS] = {
+	{8, 0, (int[8]){0, 1, 3, 11, 15, 11, 3, 1}},
+	{6, 0, (int[6]){0, 1, 2, 4, 2, 1}}
+};
+int active_program = 0;
+
+int counter = 0;
+
+int button_debounce = 0;
+
+void init()
+{
 	DDRA |= ((1<<LED_0) | (1<<LED_1) | (1<<LED_2) | (1<<LED_3));
-	// disable leds
 	PORTA &= ~((1<<LED_0) | (1<<LED_1) | (1<<LED_2) | (1<<LED_3));
+}
+
+void buttonPressed()
+{
+	active_program++;
+	active_program %= PROGRAMS;
 }
 
 int main(void)
 {
 	init();
-	//loop
-	//alternative: loop_until_bit_is_clear(WARTEPIN, WARTEBIT);
-    while (1)
-    {
-        if button(0)
-		ledOn(0);
-        if button(1)
-        	ledOff(0);
-    }
-    return (1);	// sollte nie erreicht werden
+
+	TCCR0A = (1<<(CS00|CS02));
+	TIMSK0 |= (1<<TOIE0);
+	sei();
+
+	while(1)
+	{
+		if(button_debounce)
+		{
+			if(!button(0))
+			{
+				_delay_ms(50);
+				button_debounce = 0;
+			}
+		}
+		else
+		{
+			if(button(0))
+			{
+				_delay_ms(50);
+				button_debounce = 1;
+				buttonPressed();
+			}
+		}
+	}
+
+	return 0;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	if(counter++ >= 80)
+	{
+		program_t *active = &program[active_program];
+		PORTA = active->leds[active->state];
+		active->state = (active->state + 1) % active->length;
+		counter = 0;
+	}
 }
